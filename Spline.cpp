bee2c2e432399e3930f6ca9d2c4d7655bcd64ecf
Zak Gyovai <zmeadows@gmail.com>
2012-07-26 11:11:01 -0700

    testing branch

--- Spline.cpp
+++ Spline.cpp
@@ -1,19 +1,31 @@
 #include "Spline.h"
-#include <iostream>
 
-LogSpline::LogSpline(void) {
+LogSpline::LogSpline(void) : log_min(LOGMIN) {
 
   unsigned int i;
-  const double log_min = -20;
   std::vector<double> v, logv;
-  double vi;
+  double vi, pv, pweight, step;
+
+  double isct = intersection(log_dip_polynomial, log, 1e-12,1);
 
   v.push_back(0);
   logv.push_back(log_min);
 
-  for (vi=exp(log_min) + 1e-10; vi < 1e5; vi *= 1.8) {
+  pweight = 1;
+  step = isct/10;
+  for (vi=step; vi < isct; vi+=step) {
+    v.push_back(vi);
+    pv = log_dip_polynomial(vi);
+    logv.push_back(pweight * pv + (1-pweight) * log(vi));
+    pweight *= 0.75;
+  }
+
+  pweight = 1.01;
+  while (vi < 1e12) {
     v.push_back(vi);
     logv.push_back(log(vi));
+    vi *= pweight;
+    pweight *= 1.02;
   }
 
   std::vector<double> vrev = v;
@@ -33,21 +45,47 @@ LogSpline::LogSpline(void) {
   size_t num_points = v.size();
   double* v_arr = &v[0];
   double* logv_arr = &logv[0];
-	
-	//for (int i=0;i<num_points;++i) std::cerr << v_arr[i] << " " << logv_arr[i] << std::endl;
 
   acc = gsl_interp_accel_alloc ();
-  spline = gsl_spline_alloc (gsl_interp_cspline, num_points);
+  spline = gsl_spline_alloc (gsl_interp_akima, num_points);
   gsl_spline_init (spline, v_arr, logv_arr, num_points);
 
-	/*std::cerr << std::endl;
-	std::cerr << std::endl;
-	for (vi=exp(log_min) + 1e-10; vi < 1e5; vi *= 1.8) {
-		std::cerr << vi << " " << gsl_spline_eval(spline,vi,acc) << std::endl;
-	}*/
+#ifdef OUTPUT
+  std::ofstream log_approx_graph;
+  log_approx_graph.open ("log_approx.txt");
+  for (vi=10; vi<1e11; vi *= 10) {
+    log_approx_graph << vi << " " << gsl_spline_eval(spline, vi, acc)  << std::endl;
+  }
+  log_approx_graph.close();
+#endif
+
+#ifdef DEBUG
+  std::cout << "POW/LOG INTERSECTION : " << isct << std::endl;
+  std::cout << "# SAMPLE POINTS : " << v.size() << std::endl;
+#endif
 }
 
 LogSpline::~LogSpline(void) {
   gsl_spline_free (spline);
   gsl_interp_accel_free (acc);
 }
+
+double intersection(double (*f)(double), double (*g)(double), double a, double b) {
+  unsigned int i = 1;
+  double fgp,fga,p;
+  fga = f(a) - g(a);
+
+  while (i<MAXITER) {
+    p = a + (b-a)/2;
+    fgp = f(p) - g(p);
+    if (fgp == 0 or (b-a)/2 < TOL) break;
+    i++;
+    if (fga*fgp > 0) {
+      a = p;
+      fga = fgp;
+    } else { b = p; };
+  }
+  return p;
+};
+
+double log_dip_polynomial(double v) { return (-1 * LOGMIN * 1e13 * pow(v,2.5) + LOGMIN); };
--- Spline.h
+++ Spline.h
@@ -3,15 +3,29 @@
 #include <algorithm>
 #include <math.h>
 #include <vector>
+#include <iostream>
+#include <fstream>
+
+#define TOL 1e-15
+#define MAXITER 1000
+asdf
+
+//#define DEBUG
+#define OUTPUT
 
 class LogSpline {
 private:		
   gsl_interp_accel *acc;
   gsl_spline *spline;
-  double log_min;
+  const double log_min;
+  const double pow;
 
 public:
   double operator ()(double v) { return gsl_spline_eval(spline,v,acc); };
+  double deriv(double v) { return gsl_spline_eval_deriv(spline,v,acc); };
   LogSpline(void);
   ~LogSpline(void);
 };
+
+double intersection(double (*f)(double), double (*g)(double), double a, double b);
+double log_dip_polynomial(double v);
